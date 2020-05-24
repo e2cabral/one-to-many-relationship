ed47c881744b20d3933826aa00b40a20d5f98bee
Edson Cabral Junior <e2cabraljunior@gmail.com>
2020-05-24 08:19:29 -0300

    🚧 add CarsService

---
+++ src/user/services/cars.service.ts
@@ -0,0 +1,30 @@
+import { User } from './../domain/user.model';
+import { Injectable } from '@nestjs/common';
+import { Model } from 'mongoose';
+import { Car } from '../domain/car.model';
+import { InjectModel } from '@nestjs/mongoose';
+
+@Injectable()
+export class CarsService {
+  constructor(
+    @InjectModel('Car') private readonly car: Model<Car>,
+    @InjectModel('User') private readonly user: Model<User>
+  ) {}
+
+  async get(id: string): Promise<Array<Car>> {
+    return await this.car.find({ 'user': id}).populate('user');
+  }
+
+  async getById(id: string | number): Promise<Car> {
+    return await this.car.findOne({ _id: id });
+  }
+
+  async save(car: Car, user: User): Promise<void> {
+    await new this.car(car)
+      .save()
+      .then(async (c) => {
+        user.cars.push(c._id);
+        await this.user.updateOne({ _id: user._id}, user);
+      })
+  }
+}
